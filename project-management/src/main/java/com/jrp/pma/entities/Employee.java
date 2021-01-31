758a1a190cf163b74b73f9e534a0a4a887b74e19
qzy51 <qzy51@192.168.1.234>
2021-01-31 11:06:31 -0800

    Configure properties for Postgres database

--- project-management/src/main/java/com/jrp/pma/entities/Employee.java
+++ project-management/src/main/java/com/jrp/pma/entities/Employee.java
@@ -11,12 +11,14 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Employee {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_generator")
+	@SequenceGenerator(name = "employee_generator", sequenceName = "employee_seq", allocationSize = 1)
 	private long employeeId;
 
 	private String firstName;
