4b0d826285dc4a142aef1304a0ac3084118c3d69
qzy51 <qzy51@192.168.1.234>
2021-01-31 12:03:35 -0800

    Use @SpringBootTest annotation

--- project-management/src/test/java/com/jrp/pma/dao/ProjectRepositoryIntegrationTest.java
+++ project-management/src/test/java/com/jrp/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,24 +1,21 @@
-package com.jrp.dao;
+package com.jrp.pma.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.jrp.pma.ProjectManagementApplication;
 import com.jrp.pma.dao.ProjectRepository;
 import com.jrp.pma.entities.Project;
 
-@ContextConfiguration(classes = ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({
 		@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = { "classpath:schema.sql",
 				"classpath:data.sql" }),
