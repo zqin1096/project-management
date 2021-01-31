07c6b36dd84e7113bfda62260764f598c3cf3528
qzy51 <qzy51@192.168.1.234>
2021-01-31 14:42:00 -0800

    Spring profiles

--- project-management/src/main/java/com/jrp/pma/services/ProjectService.java
+++ project-management/src/main/java/com/jrp/pma/services/ProjectService.java
@@ -3,11 +3,13 @@ package com.jrp.pma.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.jrp.pma.dao.ProjectRepository;
 import com.jrp.pma.dto.ChartData;
 import com.jrp.pma.entities.Project;
 
+@Service
 public class ProjectService {
 	@Autowired
 	ProjectRepository projectRepository;
