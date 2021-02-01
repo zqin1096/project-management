1a66497011ef607c5f30a0ea495db548dc30d6e3
qzy51 <qzy51@192.168.1.234>
2021-02-01 11:44:19 -0800

    In-memory authorization

--- project-management/src/main/java/com/jrp/pma/security/SecurityConfiguration.java
+++ project-management/src/main/java/com/jrp/pma/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.jrp.pma.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -15,13 +16,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// Define authentication mechanism.
 		// Define new user name and password. The default user name is "user".
-		auth.inMemoryAuthentication().withUser("myuser").password("pass").roles("USER").and().withUser("Zhaoyin")
-				.password("123").roles("USER");
+		auth.inMemoryAuthentication().withUser("myuser").password("pass").roles("USER").and().withUser("zhaoyin")
+				.password("pass2").roles("USER").and().withUser("managerUser").password("pass3").roles("ADMIN");
 	}
 
 	// Need a Password encoder.
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		// Specify what the logged-in user is allowed to do.
+		// The order of the antMatchers() elements is significant – the more specific
+		// rules need to come first, followed by the more general ones.
+		// formLogin() is used to support form based authentication.
+		// Only ADMIN can add new projects and employees.
+		httpSecurity.authorizeRequests().antMatchers("/projects/new").hasRole("ADMIN").antMatchers("/employees/new")
+				.hasRole("ADMIN").antMatchers("/").authenticated().and().formLogin();
 	}
 }
